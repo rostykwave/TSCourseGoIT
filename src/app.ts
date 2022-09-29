659b4026b22599d131c5bd2bfb64aaba108c1993
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-09-29 13:12:00 +0300

    s

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,10 @@
-class Key {
-  private signature: number;
-
-  constructor() {
-    this.signature = Math.random();
-  }
-
-  getSignature(): number {
-    return this.signature;
-  }
+function getPromise () {
+  return new Promise((resolve) => {
+    resolve(['Text', 50]);
+  });
 }
 
-class Person {
-  constructor(private key: Key) {}
-
-  getKey(): Key {
-    return this.key;
-  }
-}
-
-abstract class House {
-  protected door = false;
-  private tenants: Person[] = [];
-
-  constructor(protected key: Key) {}
-
-  comeIn(person: Person): void {
-    if (!this.door) {
-      throw new Error("Door is close");
-    }
-    this.tenants.push(person);
-    console.log("Person inside");
-  }
-  abstract openDoor(key: Key): boolean;
-}
-
-class MyHouse extends House {
-  openDoor(key: Key) {
-    if (key.getSignature() !== this.key.getSignature()) {
-      throw new Error("Key to another door");
-    }
-
-    return (this.door = true);
-  }
-}
-
-const key = new Key();
-// const key2 = new Key();
-
-// const house = new MyHouse(key2);
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
+getPromise ()
+.then((data) => {
+  console.log(data);
+});
--- src/interfaces_like_finction_type.ts
+++
@@ -1,8 +0,0 @@
-// // type addFunc = (n1: number, n2: number) => number;
-// interface addFunc {
-//   (n1: number, n2: number): number;
-// }
-
-// const foo: addFunc = (n1: number, n2: number) => {
-//   return n1 + n2;
-// };
